4d3ebd3109171b05d88607b2cccfaa2d759839ff
Arnat Ngawsuwan <annsw.work@gmail.com>
2023-02-19 22:19:42 +0700

    update

--- examples/basics/ex05_ADC_Read/main.c
+++ examples/basics/ex05_ADC_Read/main.c
@@ -7,21 +7,23 @@ void read_analog_0()
 
 	float adc0 = Adc_GetVoltage(0);
 
-	/*******************************************************
-	 * Adc_GetVoltage
-	 *
-	 * ####################################
-	 *
-	 * Adc_Get(uint16_t id);
-	 *
-	 * ####################################
-	 * 
-	 * Returns voltage of the target channel.
-	 * Parameter:
-	 * - id: Id of ADC (ADC_ID_0, ..., ADC_ID_3).
-	 *******************************************************/
+	float vwc;
+	if (adc0 < 1.1){
+		vwc = (10*adc0) - 1;
+	}
+	else if(adc0 >= 1.1 & adc0 <= 1.3){
+		vwc = (25*adc0) - 17.5;
+	}
+	else if(adc0 >= 1.3 & adc0 <= 1.82){
+		vwc = (48.08*adc0) - 47.5;
+	}
+	else if(adc0 >= 1.82 & adc0 <= 2.2){
+		vwc = (26.32*adc0) - 7.89;
+	}
 
-	Uart1_Printf("ADC0: %f\r\n", adc0);
+
+	Uart1_Printf("Voltage from signal: %.2f\r\n", adc0);
+	Uart1_Printf("VWC: %.2f \r", vwc);
 }
 
 int main()
