51b2bfb6b725ad73bc7bca64cf507090fdb2b331
Arnat Ngawsuwan <annsw.work@gmail.com>
2023-03-04 17:41:28 +0700

    status led

--- examples/basics/multiadc/main.c
+++ examples/basics/multiadc/main.c
@@ -26,14 +26,50 @@ void read_analog_All()
 
 	double ph;
 	double h_conc = 1e-4; // hydrogen ion concentration in moles per liter
-	ph = adc1 / (0.0592 / -log10(h_conc)); 
+	ph = adc1 / (0.0592 / -log10(h_conc));
+	if (ph > 14){
+		ph = 0;
+	}
 	Uart1_Printf("%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;\r\n", vwc, adc0, adc1, ph, adc2, adc3);
+}
+
+
+void uart1_line_received(void *evt)
+{
+
+	/**
+	 * Get line data from the event
+	 */
+	uart_line_t *ue = (uart_line_t *)evt;
+	const char *line = ue->buffer;
+
+	/**
+	 * Note: `line` variable contains `\r\n`
+	 */
+
+	Uart2_Printf("Received: %s", line);
+
+	/**
+	 * Control logic
+	 */
+	if (!strcmp(line, "led,0,1\r\n"))
+	{
+		Uart2_Printf("LED1 ON");
+		Led0_Set();
+	}
+	else if (!strcmp(line, "led,0,0\r\n"))
+	{
+		Uart2_Printf("LED1 OFF");
+		Led0_Clr();
+	}
 }
 
 int main()
 {
 	System_Init();
 	Adc_Init(); // Init ADC Function
+	Uart1_Init(115200, 64, 64);
+	Uart1_SetRxLineCallback(uart1_line_received, 128);
 	Timer_Create(1000, read_analog_All);
 	System_Start();
 }
