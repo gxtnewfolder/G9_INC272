f5a86b235e4f3160aaf65bd0222a12270ba42ada
Arnat Ngawsuwan <annsw.work@gmail.com>
2023-03-06 01:50:09 +0700

    Final

--- examples/basics/multiadc/main.c
+++ examples/basics/multiadc/main.c
@@ -5,23 +5,30 @@
 void read_analog_All()
 {
 
-	float adc0 = Adc_GetVoltage(0);
+	double adc0 = Adc_GetVoltage(0);
 	double adc1 = Adc_GetVoltage(1);
-	float adc2 = Adc_GetVoltage(2);
-	float adc3 = Adc_GetVoltage(3);
 
-	float vwc;
+	double vwc;
 	if (adc0 < 1.1){
-		vwc = (10*adc0) - 1;
+		vwc = (10*adc0) - 1; // 0.0 - 10.0
 	}
-	else if(adc0 >= 1.1 & adc0 <= 1.3){
-		vwc = (25*adc0) - 17.5;
+	else if(adc0 >= 1.1 && adc0 <= 1.3){
+		vwc = (25*adc0) - 17.5; // 10.0 - 15.4
 	}
-	else if(adc0 >= 1.3 & adc0 <= 1.82){
-		vwc = (48.08*adc0) - 47.5;
+	else if(adc0 >= 1.3 && adc0 <= 1.82){
+		vwc = (48.08*adc0) - 47.5; // 15.4 - 40.2
 	}
-	else if(adc0 >= 1.82 & adc0 <= 2.2){
-		vwc = (26.32*adc0) - 7.89;
+	else if(adc0 >= 1.82 && adc0 <= 2.2){
+		vwc = (26.32*adc0) - 7.89; // 40.2 - 50.0
+	}
+	else if(adc0 >= 2.2 && adc0 <= 2.5){
+		vwc = (48.78*adc0) - 52; // 50.0 - 70.0
+	}
+	else if(adc0 >= 2.5 && adc0 <= 3.3){
+		vwc = (26.32*adc0) + 4; // 70.0 - 100.0
+	}
+	else{
+		vwc = 0;
 	}
 
 	double ph;
@@ -30,28 +37,15 @@ void read_analog_All()
 	if (ph > 14){
 		ph = 0;
 	}
-	Uart1_Printf("%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;\r\n", vwc, adc0, adc1, ph, adc2, adc3);
+	Uart1_Printf("%.2f;%.2f;%.2f;%.2f;\r\n", adc0, vwc, adc1, ph);
 }
 
 
 void uart1_line_received(void *evt)
 {
-
-	/**
-	 * Get line data from the event
-	 */
 	uart_line_t *ue = (uart_line_t *)evt;
 	const char *line = ue->buffer;
 
-	/**
-	 * Note: `line` variable contains `\r\n`
-	 */
-
-	Uart2_Printf("Received: %s", line);
-
-	/**
-	 * Control logic
-	 */
 	if (!strcmp(line, "led,0,1\r\n"))
 	{
 		Uart2_Printf("LED1 ON");
@@ -62,6 +56,26 @@ void uart1_line_received(void *evt)
 		Uart2_Printf("LED1 OFF");
 		Led0_Clr();
 	}
+	else if (!strcmp(line, "led,1,2\r\n"))
+	{
+		Uart2_Printf("LED2 ON");
+		Led1_Inv();
+	}
+	else if (!strcmp(line, "led,1,0\r\n"))
+	{
+		Uart2_Printf("LED2 OFF");
+		Led1_Clr();
+	}
+	else if (!strcmp(line, "led,2,2\r\n"))
+	{
+		Uart2_Printf("LED3 ON");
+		Led2_Inv();
+	}
+	else if (!strcmp(line, "led,2,0\r\n"))
+	{
+		Uart2_Printf("LED3 OFF");
+		Led2_Clr();
+	}
 }
 
 int main()
@@ -69,6 +83,7 @@ int main()
 	System_Init();
 	Adc_Init(); // Init ADC Function
 	Uart1_Init(115200, 64, 64);
+	// Uart2_Init(115200, 64, 64);
 	Uart1_SetRxLineCallback(uart1_line_received, 128);
 	Timer_Create(1000, read_analog_All);
 	System_Start();
